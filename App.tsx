db141f461f32569dd878bcd6e902925ab735839d
Leandro Vidal <leandrovidal56@gmail.com>
2022-02-21 20:12:43 -0300

    input_sentry_dns

--- App.tsx
+++ App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import * as Sentry from "@sentry/react-native";
 
+import * as TesteSentry from 'react-native-dotenv';
+
+const { SENTRY } = process.env;
+
 import { Home } from './src/pages/Home';
 
 
-
-
 export default function App() {
-    Sentry.init({
-      dsn: process.env.SENTRY,
-      tracesSampleRate: 1.0,
-    });
+  Sentry.init({
+    dsn: process.env.SENTRY,
+    tracesSampleRate: 1.0,
+  });
+  console.log('PEGAR LOG', process.env.SENTRY)
+  
     
     return (
       <>
@@ -24,3 +28,15 @@ export default function App() {
     </>
   );
 }
+
+
+
+
+
+
+
+
+
+    
+    
+    
